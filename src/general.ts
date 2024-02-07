797c6966202084a5d75c6afc84a0cf1d89d7ab32
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-02-07 17:51:21 -0500

    Mathis feat: typing and methods for all categories

---
+++ src/general.ts
@@ -0,0 +1,18 @@
+export type Coordinates = {
+    lat: number,
+    lng: number
+};
+
+export function validCoordinates(coord: Coordinates) {
+    const { lat, lng } = coord;
+    const respectLimits = lat >= -90 && lat <= 90 && lng >= -180 && lng <= 180;
+    const notNull = lat != null && lng != null;
+    const notNaN = !isNaN(lat) && !isNaN(lng);
+    
+    if ( !(respectLimits && notNull && notNaN) ) {
+        console.log(`the coordinates (lat: ${coord.lat}, lng: ${coord.lng}) do not respect the template of coordinates`);
+    }
+    return respectLimits && notNull && notNaN;
+};
+
+export type MapType = "AIR_QUALITY" | "SOLAR";
