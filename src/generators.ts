a8a7488a5637dafb60b61f77a60aa0c7ef63ab6e
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-03-01 17:09:24 -0500

    Mathis feat: Will also export constants

--- src/generators.ts
+++ src/generators.ts
@@ -1,7 +1,10 @@
-import UtilGen from "./generators/utilGenerator";
+import UtilGen, { UtilGeneratorConstants as UtilGenConstants } from "./generators/utilGenerator";
 import StringGen from "./generators/stringGenerator";
-import NumberGen from "./generators/numberGenerator";
+import NumberGen, { NumberGeneratorConstants as NumberGenConstants } from "./generators/numberGenerator";
 
 export const UtilGenerator = UtilGen;
 export const StringGenerator = StringGen;
 export const NumberGenerator = NumberGen;
+
+export const UtilGeneratorConstants = UtilGenConstants;
+export const NumberGeneratorConstants = NumberGenConstants;
